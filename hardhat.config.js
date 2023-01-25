7097a6fb9f2dadc8826bce81b007c5e34de456f7
mulili <mulili.nzuki@gmail.com>
2023-01-25 13:13:04 +0300

    added OZ contract deps

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,45 @@
-require("@nomicfoundation/hardhat-toolbox");
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require('dotenv').config();
+require('@nomiclabs/hardhat-waffle');
+require("hardhat-deploy");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+ defaultNetwork: "hardhat",
+ solidity: {
+   compilers: [
+     {
+       version: "0.8.17",
+       settings: {
+         optimizer: {
+           enabled: true,
+           runs: 200
+         }
+       }
+     },
+    
+   ],
+ },
+ networks: {
+   hardhat: {
+     forking: {
+       url: process.env.RPC_NODE_URL,
+       blockNumber: 16015939,
+     }
+   },
+   mainnet: {
+     accounts: [`0x${process.env.MAINNET_PRIVATE_KEY}`],
+     url: process.env.RPC_NODE_URL,
+     chainId: 1,
+     timeout: 2000000
+   }
+ },
+ namedAccounts: {
+   deployer: 0,
+ },
+ mocha: {
+   timeout: 2000000
+ }
+
 };
